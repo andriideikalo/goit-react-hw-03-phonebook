eb08038653aed35eeaabd1b5aff7be1b063ba2a1
andriideikalo <wlada801@gmail.com>
2023-01-22 16:36:31 +0200

    add Notiflix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,15 +36,8 @@ export class App extends Component {
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
     }
-    // або початкові дані, коли localStorage ще пустий
-    this.setState({
-      contacts: [
-        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      ],
-    });
+    alert(`No contacts found`);
+    return;
   }
   // ---------------------------
 
